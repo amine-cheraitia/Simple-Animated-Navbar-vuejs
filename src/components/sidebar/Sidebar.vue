1c66e65a3372848596a9187c87605d10c9d3e956
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-04-23 15:15:15 +0200

    first commit

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="sidebar" :style="{ width: sidebarWith }">ss</div>
+</template>
+
+<script>
+export default {
+	name: "name",
+	props: {},
+	computed: {
+		sidebarWith() {
+			console.log(this.$store.getters.Sidebarwiths);
+			return this.$store.getters.Sidebarwiths;
+		},
+	},
+};
+</script>
+<style>
+:root {
+	--sidebar-bg-color: #2f855a;
+	--sidebar-item-hover: #38a169;
+	--sidebar-item-active: #276749;
+}
+</style>
+<style scoped>
+.sidebar {
+	color: white;
+	background-color: var(--sidebar-bg-color);
+
+	float: left;
+	position: fixed;
+	z-index: 1;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	padding: 0.5em;
+
+	transition: 0.3s ease;
+
+	display: flex;
+	flex-direction: column;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createStore } from "vuex";
 
-createApp(App).use(router).mount('#app')
+import "@fortawesome/fontawesome-free/js/all";
+
+const store = createStore({
+	state() {
+		return {
+			collapsed: false,
+			SIDEBAR_WITH: 180,
+			SIDEBAR_WITH_COLLAPSED: 38,
+		};
+	},
+	mutations: {
+		/* 		Sidebarwiths(stat) {
+			if (collapsed == false) {
+				stat.current_width = stat.SIDEBAR_WITH;
+			} else {
+				stat.current_width = stat.SIDEBAR_WITH_COLLAPSED;
+			}
+		}, */
+	},
+	getters: {
+		/* 	collapsed(state) {
+			return state.collapsed;
+		}, */
+		Sidebarwiths(stat) {
+			if (stat.collapsed == false) {
+				return `${stat.SIDEBAR_WITH}px`;
+			} else {
+				return `${stat.SIDEBAR_WITH_COLLAPSED}px`;
+			}
+		},
+		SIDEBAR_WITH(stat) {
+			return stat.SIDEBAR_WITH;
+		},
+		SIDEBAR_WITH_COLLAPSED(stat) {
+			return stat.SIDEBAR_WITH_COLLAPSED;
+		},
+	},
+	actions: {},
+});
+
+/* const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app"); */
+createApp(App).use(store).use(router).mount("#app");
